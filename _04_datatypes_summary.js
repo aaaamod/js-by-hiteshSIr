8976cbe5eb8f7a8c86e72f81812465e23e06246b
aaaamod <tg1811931@gmail.com>
2025-07-18 18:26:05 +0530

    datatype_summary

---
+++ _04_datatypes_summary.js
@@ -0,0 +1,27 @@
+let a=23;
+let b =23.3
+let c=true;
+let d=null
+let e;
+
+let f=Symbol("123");
+let g=Symbol("123");
+
+console.log(f);
+console.log(g);
+console.log(g==f);
+//even if value inside symbol is same...unique hai f and g....aur return type symbol hi hota.
+
+
+let h=993072080838n
+
+const hero=["ss", "kda","wkj"]
+let objj={
+  aA:"lfs",
+  dl:"kdl"
+}
+
+const myFunction=function(){
+  console.log("function");
+  
+}
