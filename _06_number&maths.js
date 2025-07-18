95ecc48dec720f407b60da3765851bb092e9e79e
aaaamod <tg1811931@gmail.com>
2025-07-19 03:50:03 +0530

    number&maths

---
+++ _06_number&maths.js
@@ -0,0 +1,30 @@
+const s=39;
+const t=new Number(399);
+// console.log(t);
+// console.log(s);
+// console.log(typeof s);
+// console.log(typeof t);
+// console.log(t.toString().length);
+// console.log(t.toFixed(2));
+// console.log(23323.9224.toPrecision(3));
+// console.log(23323.9224.toPrecision(5));
+// console.log(Number.MAX_VALUE);   
+// console.log(Number.MAX_SAFE_INTEGER);   
+// console.log(Number.length);
+
+
+//++++++++++++++++++++++++++ Maths ++++++++++++//
+
+   
+console.log(Math);
+
+
+console.log(Math.abs(-44));
+console.log(Math.round(4.3));
+console.log(Math.ceil(4.3));
+console.log(Math.floor(4.3));
+console.log(Math.max(3, 7, 1));
+console.log(Math.random());
+
+
+
