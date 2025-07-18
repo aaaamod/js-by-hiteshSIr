77bf5b6620a97d62c8c1c0a3148514279640eced
aaaamod <tg1811931@gmail.com>
2025-07-18 22:02:51 +0530

    strings-detail

---
+++ _05_strings.js
@@ -0,0 +1,18 @@
+const name="aamod"
+
+const anotherName= new String("aamir");
+console.log(anotherName.toUpperCase());
+
+console.log(typeof name);
+console.log(typeof anotherName);
+console.log(anotherName.String);
+console.log(anotherName.__proto__);
+console.log(anotherName.charAt(2));
+console.log(anotherName.indexOf('i'));
+console.log(anotherName.substring(0,3));
+console.log(anotherName.slice(-4,2));
+console.log("  dlld   ".trim());
+console.log("hwlhq%20dsklknfaf".replace("%20","-"));
+console.log("lffsa-fasml-sflf".split("-"));
+
+
