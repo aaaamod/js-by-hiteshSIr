8a9d9a45dd2eeccec4631ec50592d618f17fc08d
aaaamod <tg1811931@gmail.com>
2025-07-24 04:21:33 +0530

    add array basics and methods

---
+++ basics_2/01_arrays.js
@@ -0,0 +1,28 @@
+const myArray=[0,1,2,3]
+
+// const myArray2=new Array(1,2,3,4)
+
+
+// myArray.push(5)
+// myArray.push(8)
+// myArray.pop()
+// myArray.unshift(9)//daal deta first me 
+// myArray.shift()//nikal leta first se
+
+// const arr=myArray.join()
+// console.log(myArray);
+// console.log(arr);//give array but type is string
+// console.log(myArray.includes(9));
+// console.log(myArray.indexOf(17));
+
+
+
+console.log("a ", myArray );
+
+const myna=myArray.slice(1,3)
+
+console.log("b " , myArray);
+
+const myna2=myArray.splice(1,3)//original se hatado aur jo hata use return karo
+console.log("c " , myArray);
+console.log(myna2);
