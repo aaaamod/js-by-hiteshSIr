e766a06d54eeaf74ef759decac0ec462fb3c44cb
aaaamod <tg1811931@gmail.com>
2025-07-25 02:35:17 +0530

    array-2

---
+++ basics_2/02_array.js
@@ -0,0 +1,29 @@
+const aa=["a", "b","c"];
+const bb=["d", "e","f"];
+
+//aa.push(bb)
+
+// console.log(aa);
+
+// let cc=aa.concat(bb)
+// console.log(cc);
+
+
+// const all_new_value=[...aa , ...bb];
+// console.log(all_new_value);
+
+
+// const another_array=[1,2,3,[4,5],6,7,[8,9,[6,3]]]
+
+// const l=another_array.flat(2)
+// console.log(l);
+
+// console.log(Array.isArray("fslfas"));
+// console.log(Array.from("fslfas"));
+// console.log(Array.from({name:"hitesh"}));
+
+let s1=100;
+let s2=200;
+let s3=300;
+
+console.log(Array.of(s1,s2,s3));
