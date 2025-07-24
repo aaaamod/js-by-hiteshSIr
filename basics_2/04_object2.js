e76efc693e51a512bbde493ee550b6eef181e73f
aaaamod <tg1811931@gmail.com>
2025-07-25 05:06:58 +0530

    add object-2

---
+++ basics_2/04_object2.js
@@ -0,0 +1,42 @@
+const newUser=new Object()
+
+newUser.id="344144"
+newUser.name="sammy"
+
+console.log(newUser.hasOwnProperty("id"));
+
+// console.log(Object.entries(newUser));
+
+
+// console.log(Object.keys(newUser));
+// console.log(Object.values(newUser));
+
+
+// console.log(newUser);
+
+// const regularUser={
+//   email:"skll@gmail.com",
+//   fullname:{
+//     first:"vaibhav",
+//     last:"shukla"
+//   }
+// }
+
+// const obj1={
+// 1:"a" ,2:"b" 
+// }
+// const obj2={
+// 3:"a" ,4:"b" 
+// }
+
+// const obj3={...obj1 , ...obj2}
+
+// console.log(obj3);
+
+
+
+// const obj3=Object.assign({},obj1, obj2)
+
+// console.log(obj1);
+// console.log(obj3);
+
