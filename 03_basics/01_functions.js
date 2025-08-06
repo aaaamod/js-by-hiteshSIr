53703408bf42bddd1e021e49f75b39418bd13684
aaaamod <tg1811931@gmail.com>
2025-08-06 13:02:00 +0530

    add function

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,52 @@
+function sayMyName(){
+  console.log("vaibhav");
+}
+
+// sayMyName();
+
+// function addToNumers(n01 , n02){
+//   console.log(n01+n02);
+  
+// }
+
+// function addToNumers(no1 , no2){
+//   return no1+no2;
+// }
+// let a=(addToNumers(3, 4));
+
+// console.log("result =",a);
+
+// function loginUserMessage(username="sam"){
+// return `${username} just logged in`
+// }
+
+// console.log(loginUserMessage(""));
+
+
+// function calculateCartPrice(...nums){
+// return nums
+// }
+
+// console.log(calculateCartPrice(1, 2,3,4));
+
+
+// const user={
+//   username:"vaibhav",
+//   price:199
+// }
+
+// function handleObject(anyObject){
+//   console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+  
+// }
+
+// handleObject(user)
+
+// const myNewArray= [100 , 200 , 300];
+
+// function returnSecondValue(arr){
+//   return arr[1];
+// }
+
+// console.log(returnSecondValue(myNewArray));
+// console.log(returnSecondValue([300 , 500 , 100]));
