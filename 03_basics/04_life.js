e95e0efd0ea3e2ffd8ab6d1225f43ac75133141a
aaaamod <tg1811931@gmail.com>
2025-08-07 11:01:02 +0530

    add iife

---
+++ 03_basics/04_life.js
@@ -0,0 +1,21 @@
+(function chai(){
+  console.log("DB Connected");
+  
+})();
+
+
+(function chai2(name){
+  //named iife
+  console.log("dl "+ name);
+  
+})("vaibhav");
+
+
+((name)=>{
+  //unnamed iife
+console.log(name);
+
+})("vaibhav")
+
+
+
